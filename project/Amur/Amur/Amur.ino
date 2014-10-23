866025daa07403923c68f8eed2d2fc85415f20ae
Hiron Roy <me@hironroy.com>
2014-10-23 19:57:41 -0400

    Cleaned up Amur.ino

--- project/Amur/Amur/Amur.ino
+++ project/Amur/Amur/Amur.ino
@@ -50,16 +50,7 @@
 //byte m_clear = 5;    //Master Clear button
 //byte m_rec = 6;      //Master Record
 //byte m_backout1 = 8; //Backout for Channel 1
-//byte m_morse1 = 10;  //Input for Channel 1
 //byte m_backout2 = 11;//Backout for Channel 2
-//byte m_morse2  = 13; //Input for Channel 2
-//
-//byte m_metronome = A0;  //Analog input for Metronome Speed
-//
-////Outputs
-//byte m_metronome_led = 7;  //Metronome Blinking
-//byte m_morse_led1 = 9;     // morse 1
-//byte m_morse_led2 = 12 ;   // morse 2
 
 
 Hitter hitter1;
@@ -73,31 +64,22 @@ void runHitter2();
 
 void setup() {
     Serial.begin(9600);      // open the serial port at 9600 bps:
+    
+    //hitters init
+    
+    // duration of hit (ms), input pin, output pin
     hitter1.begin(100, 10, 9);
     hitter2.begin(100, 13, 12);
 
     tBeat.init();
     
+    //All of the actuators get triggered at the sample interval
     tBeat.newHook(amurIO.sampleInterval, runInterval);
     tBeat.newHook(amurIO.sampleInterval, runHitter1);
     tBeat.newHook(amurIO.sampleInterval, runHitter2);
 
 
-    
     tBeat.start();
-
-
-    //Setting up Pins
-//    pinMode(m_clear,INPUT);
-//    pinMode(m_rec,INPUT);
-//    pinMode(m_backout1,INPUT);
-//    pinMode(m_morse1,INPUT);
-//    pinMode(m_backout2,INPUT);
-//    pinMode(m_morse2,INPUT);
-//    
-//    pinMode(m_metronome_led, OUTPUT);
-//    pinMode(m_morse_led1, OUTPUT);
-//    pinMode(m_morse_led2, OUTPUT);
     
 }
 
@@ -114,34 +96,6 @@ void runHitter2(){
 void loop() {
     
     tBeat.exec();
-
-    
-//    //Testing Potentiometer and Metronome Led
-//    sensorValue = analogRead(m_metronome);
-//    // turn the ledPin on
-//    digitalWrite(m_metronome_led, HIGH);
-//    // stop the program for <sensorValue> milliseconds:
-//    delay(sensorValue);
-//    // turn the ledPin off:
-//    digitalWrite(m_metronome_led, LOW);
-//    // stop the program for for <sensorValue> milliseconds:
-//    delay(sensorValue);
-    
-    //Testing morse button and LED (remember delays from previous potentiometer
-//    if(digitalRead(m_morse1) == LOW){
-//        digitalWrite(m_morse_led1, HIGH);
-//    }
-//    else{
-//        digitalWrite(m_morse_led1, LOW);
-//    }
-//    
-//    if(digitalRead(m_morse2) == LOW){
-//        digitalWrite(m_morse_led2, HIGH);
-//    }
-//    else{
-//        digitalWrite(m_morse_led2, LOW);
-//    }
-    
     
 }
 
